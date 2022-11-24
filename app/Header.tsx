ed99297d65b99e23d06d306ad74839e71f5c3796
owuraku-zenas <awukuzenas@gmail.com>
2022-11-24 01:16:34 +0000

    Implementing firebase Google Auth

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import Image from 'next/image'
+import type { ImageLoaderProps } from 'next/image';
 import Link from 'next/link'
 import React from 'react'
 import logo from "./../public/logo.png"
@@ -8,6 +9,8 @@ import { HiSearch } from "react-icons/hi";
 import { AiOutlineBell } from "react-icons/ai"
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '../utils/firebase'
 
 type Props = {}
 
@@ -16,6 +19,9 @@ function classNames(...classes: string[]) {
 }
 
 const Header = (props: Props) => {
+
+  const [user, loading] = useAuthState(auth)
+
   return (
     <header className='bg-white h-14 border-b shadow'>
       <div className='flex flex-row justify-between max-w-[1240px] m-auto h-full p-1'>
@@ -38,122 +44,134 @@ const Header = (props: Props) => {
               <HiSearch className='hover:bg-blue-100 hover:text-blue-600' size={25} />
             </div>
           </Link>
-          <Link href={"/login"}>
-            <button className='h-full border border-blue-600 font-semibold text-blue-600 rounded-md px-4 hover:underline hover:text-white hover:bg-blue-600'>Login</button>
-          </Link>
-          <Link href={"/new"}>
-            <button className='h-full border border-blue-600 font-semibold text-blue-600 rounded-md px-4 hover:underline hover:text-white hover:bg-blue-600 hidden md:block'>Create Post</button>
-          </Link>
-          <Link href={"/notifications"}>
-            <div className='flex items-center justify-center rounded-md h-full w-14 hover:bg-blue-100'>
-              <AiOutlineBell className='hover:bg-blue-100 hover:text-blue-600' size={25} />
-            </div>
-          </Link>
-          <Menu as="div" className="relative inline-block text-left">
-            <div className='w-12'>
-              <Menu.Button className="inline-flex w-full justify-center rounded-full object-cover overflow-hidden border hover:bg-blue-100 hover:ring-4 border-gray-300 bg-white shadow-sm focus:outline-none">
-                <Image src={logo} alt={"user photo"} />
-              </Menu.Button>
-            </div>
+          {
+            !user && (
+              <Link href={"/login"}>
+                <button className='h-full border border-blue-600 font-semibold text-blue-600 rounded-md px-4 hover:underline hover:text-white hover:bg-blue-600'>Login</button>
+              </Link>
+            )
+          }
+          {
+            user && (
+              <>
+                <Link href={"/new"}>
+                  <button className='h-full border border-blue-600 font-semibold text-blue-600 rounded-md px-4 hover:underline hover:text-white hover:bg-blue-600 hidden md:block'>Create Post</button>
+                </Link>
+                <Link href={"/notifications"}>
+                  <div className='flex items-center justify-center rounded-md h-full w-14 hover:bg-blue-100'>
+                    <AiOutlineBell className='hover:bg-blue-100 hover:text-blue-600' size={25} />
+                  </div>
+                </Link>
+                <Menu as="div" className="relative inline-block text-left">
+                  <div className='w-12'>
+                    <Menu.Button className="inline-flex w-full justify-center rounded-full object-cover overflow-hidden border hover:bg-blue-100 hover:ring-4 border-gray-300 bg-white shadow-sm focus:outline-none">
+                      {/* FIXME: Find solution for this error */}
+                      <Image src={user?.photoURL} alt={"user photo"} width={48} height={48} />
+                      {/* <img src={user.photoURL} alt="" srcset="" /> */}
+                    </Menu.Button>
+                  </div>
 
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
-            >
-              <Menu.Items className="absolute right-0 z-10 mt-2 p-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <div className="py-1">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href={"/[user_profile]"}
-                        className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
-                        )}
-                      >
-                        <h3 className='font-semibold'>Zenas Kwaku Awuku</h3>
-                        <p>@owurakuzenas</p>
-                      </Link>
-                    )}
-                  </Menu.Item>
-                </div>
-                <div className="py-1">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/dashboard"
-                        className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
-                        )}
-                      >
-                        Dashboard
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/new"
-                        className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
-                        )}
-                      >
-                        Create Post
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/dashboard"
-                        className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
-                        )}
-                      >
-                        Bookmarks
-                      </Link>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/settings"
-                        className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
-                        )}
-                      >
-                        Settings
-                      </Link>
-                    )}
-                  </Menu.Item>
-                </div>
-                <div className="py-1">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <Link
-                        href="/signout"
-                        className={classNames(
-                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                          'block px-4 py-2 text-sm'
-                        )}
-                      >
-                        Sign Out
-                      </Link>
-                    )}
-                  </Menu.Item>
-                </div>
-              </Menu.Items>
-            </Transition>
-          </Menu>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 p-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <div className="py-1">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              href={"/[user_name]"}
+                              className={classNames(
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                'block px-4 py-2 text-sm'
+                              )}
+                            >
+                              <h3 className='font-semibold'>{user.displayName}</h3>
+                              <p>[user_name]</p>
+                            </Link>
+                          )}
+                        </Menu.Item>
+                      </div>
+                      <div className="py-1">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              href="/dashboard"
+                              className={classNames(
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                'block px-4 py-2 text-sm'
+                              )}
+                            >
+                              Dashboard
+                            </Link>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              href="/new"
+                              className={classNames(
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                'block px-4 py-2 text-sm'
+                              )}
+                            >
+                              Create Post
+                            </Link>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              href="/dashboard"
+                              className={classNames(
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                'block px-4 py-2 text-sm'
+                              )}
+                            >
+                              Bookmarks
+                            </Link>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <Link
+                              href="/settings"
+                              className={classNames(
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                'block px-4 py-2 text-sm'
+                              )}
+                            >
+                              Settings
+                            </Link>
+                          )}
+                        </Menu.Item>
+                      </div>
+                      <div className="py-1">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <div
+                              onClick={() => auth.signOut()}
+                              className={classNames(
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                'block px-4 py-2 text-sm'
+                              )}
+                            >
+                              Sign Out
+                            </div>
+                          )}
+                        </Menu.Item>
+                      </div>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
+              </>
+            )
+          }
         </div>
       </div>
     </header>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,31 +1,59 @@
-import React from 'react'
-import { AiFillApple, AiFillFacebook, AiFillMail, AiOutlineMail } from 'react-icons/ai'
-import {FcGoogle} from 'react-icons/fc'
-import {BsGithub, BsMicrosoft, BsTwitter} from 'react-icons/bs'
-import {GoMail} from 'react-icons/go'
+'use client'
+
+import React, { useEffect } from 'react'
+import { AiFillApple, AiFillFacebook } from 'react-icons/ai'
+import { FcGoogle } from 'react-icons/fc'
+import { BsGithub, BsMicrosoft, BsTwitter } from 'react-icons/bs'
+import { GoMail } from 'react-icons/go'
+import { useRouter } from 'next/navigation'
+
+import { auth } from '../../utils/firebase'
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { useAuthState } from 'react-firebase-hooks/auth'
 
 type Props = {}
 
 const page = (props: Props) => {
-  return (
-    <div className='h-full flex items-center justify-center w-full md:h-[800px]'>
-        <div className="bg-white md:max-w-[650px] md:h-auto h-full w-full flex flex-col items-center justify-center md:justify-start p-10 text-center sm:px-20 md:shadow-xl md:rounded-lg">
-            <h2 className='font-bold text-[40px]'>Welcome to Next Buzz</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing</p>
-            <div className="py-6 w-full flex flex-col items-center gap-4">
-                {/* FIXME: Align Logos and Texts with Grid */}
-                <button className='flex gap-2 items-center w-full p-4 rounded-lg text-lg justify-center bg-black text-white hover:shadow-2xl ease-in-out duration-200'> <AiFillApple className='text-white' size={30} /> Continue with Apple</button>
-                <button className='flex gap-2 items-center w-full p-4 rounded-lg text-lg justify-center bg-[#333] text-white hover:shadow-2xl ease-in-out duration-200'> <BsGithub className='text-white' size={30} /> Continue with GitHub</button>
-                <button className='flex gap-2 items-center w-full p-4 rounded-lg text-lg justify-center bg-[#4285F4] text-white hover:shadow-2xl ease-in-out duration-200'> <FcGoogle size={30} /> Continue with Google</button>
-                <button className='flex gap-2 items-center w-full p-4 rounded-lg text-lg justify-center bg-[#1DA1F2] text-white hover:shadow-2xl ease-in-out duration-200'> <BsTwitter className='text-white' size={30} /> Continue with Twitter</button>
-                <button className='flex gap-2 items-center border w-full p-4 rounded-lg text-lg justify-center hover:shadow-2xl ease-in-out duration-200'> <AiFillFacebook className='text-[#4267B2]' size={30} /> Continue with Facebook</button>
-                <button className='flex gap-2 items-center border w-full p-4 rounded-lg text-lg justify-center bg-white text-black hover:shadow-2xl ease-in-out duration-200'> <BsMicrosoft className='text-[#737373]' size={30} /> Continue with Microsoft</button>
-                <button className='flex gap-2 items-center border w-full p-4 rounded-lg text-lg justify-center bg-white text-black hover:shadow-2xl ease-in-out duration-200'> <GoMail size={30} /> Continue with Email</button>
-                
+
+    const [user, loading] = useAuthState(auth)
+
+    const googleProvider = new GoogleAuthProvider();
+    const router = useRouter();
+    const googleLogin = async () => {
+        try {
+            const result = await signInWithPopup(auth, googleProvider);
+            console.log(result.user)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        if (user) {
+            router.back()
+        }
+
+    }, [user])
+
+    return (
+        <div className='h-full flex items-center justify-center w-full md:h-[800px]'>
+            <div className="bg-white md:max-w-[650px] md:h-auto h-full w-full flex flex-col items-center justify-center md:justify-start p-10 text-center sm:px-20 md:shadow-xl md:rounded-lg">
+                <h2 className='font-bold text-[40px]'>Welcome to Next Buzz</h2>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing</p>
+                <div className="py-6 w-full flex flex-col items-center gap-4">
+                    {/* FIXME: Align Logos and Texts with Grid */}
+                    <button className='flex gap-2 items-center w-full p-4 rounded-lg text-lg justify-center bg-black text-white hover:shadow-2xl ease-in-out duration-200'> <AiFillApple className='text-white' size={30} /> Continue with Apple</button>
+                    <button className='flex gap-2 items-center w-full p-4 rounded-lg text-lg justify-center bg-[#333] text-white hover:shadow-2xl ease-in-out duration-200'> <BsGithub className='text-white' size={30} /> Continue with GitHub</button>
+                    <button onClick={googleLogin} className='flex gap-2 items-center w-full p-4 rounded-lg text-lg justify-center bg-[#4285F4] text-white hover:shadow-2xl ease-in-out duration-200'> <FcGoogle size={30} /> Continue with Google</button>
+                    <button className='flex gap-2 items-center w-full p-4 rounded-lg text-lg justify-center bg-[#1DA1F2] text-white hover:shadow-2xl ease-in-out duration-200'> <BsTwitter className='text-white' size={30} /> Continue with Twitter</button>
+                    <button className='flex gap-2 items-center border w-full p-4 rounded-lg text-lg justify-center hover:shadow-2xl ease-in-out duration-200'> <AiFillFacebook className='text-[#4267B2]' size={30} /> Continue with Facebook</button>
+                    <button className='flex gap-2 items-center border w-full p-4 rounded-lg text-lg justify-center bg-white text-black hover:shadow-2xl ease-in-out duration-200'> <BsMicrosoft className='text-[#737373]' size={30} /> Continue with Microsoft</button>
+                    <button className='flex gap-2 items-center border w-full p-4 rounded-lg text-lg justify-center bg-white text-black hover:shadow-2xl ease-in-out duration-200'> <GoMail size={30} /> Continue with Email</button>
+
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default page
